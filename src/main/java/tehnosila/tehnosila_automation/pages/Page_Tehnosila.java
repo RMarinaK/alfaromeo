b435c2c190f398949840877d58c4a277d8ecaa4d
Quyxor <unknowfull@gmail.com>
2016-06-14 13:25:55 +0300

    Add files via upload

--- src/main/java/tehnosila/tehnosila_automation/pages/Page_Tehnosila.java
+++ src/main/java/tehnosila/tehnosila_automation/pages/Page_Tehnosila.java
@@ -20,358 +20,440 @@ import org.slf4j.LoggerFactory;
  *
  */
 
-public class Page_Tehnosila extends PagesBase{
+public class Page_Tehnosila extends PagesBase {
 	private static Logger Log = LoggerFactory.getLogger(Page_Tehnosila.class);
-	//DSE: url to check page
+	// DSE: url to check page
 	protected String URL_MATCH = super.getBaseURL();
-	
+
 	@FindBy(id = "all-goods")
 	public WebElement allgoods; // Каталог товаров
-	
-	@FindBy(xpath="//div[@class='cat-top-level']/ul/li/a[contains(text(),'Телевизоры, аудио, видео')]")
+
+	@FindBy(xpath = "//div[@class='cat-top-level']/ul/li/a[contains(text(),'Телевизоры, аудио, видео')]")
 	private WebElement tvaudiovideo; // Телевизоры, аудио, видео
-	
-	@FindBy(xpath="//div[@id='category-172']/ul/li/a[contains(text(),'Телевизоры')]")
+
+	@FindBy(xpath = "//div[@id='category-172']/ul/li/a[contains(text(),'Телевизоры')]")
 	private WebElement tv; // Телевизоры
-	
-	@FindBy(xpath="//div[@id='category-364']/ul/li/a[contains(text(),'LED телевизоры')]")
+
+	@FindBy(xpath = "//div[@id='category-364']/ul/li/a[contains(text(),'LED телевизоры')]")
 	private WebElement ledtv; // LED телевизоры
-	
-	@FindBy(xpath="//a[contains(text(),'О компании Техносила')]")
+
+	@FindBy(xpath = "//a[contains(text(),'О компании Техносила')]")
 	private WebElement aboutTC; // О компании Техносила
-	
-	@FindBy(xpath="//a[contains(text(),'Новости компании')]")
+
+	@FindBy(xpath = "//a[contains(text(),'Новости компании')]")
 	private WebElement newsTC; // Новости компании
-	
-	@FindBy(xpath="//a[contains(text(),'Вакансии')]")
+
+	@FindBy(xpath = "//a[contains(text(),'Вакансии')]")
 	private WebElement career; // Вакансии
-	
-	@FindBy(xpath="//a[contains(text(),'Пресс-релизы')]")
+
+	@FindBy(xpath = "//a[contains(text(),'Пресс-релизы')]")
 	private WebElement press; // Пресс-релизы
-	
-	@FindBy(xpath="//a[contains(text(),'Контактная информация')]")
+
+	@FindBy(xpath = "//a[contains(text(),'Контактная информация')]")
 	private WebElement contacts; // Контактная информация
-	
-	@FindBy(xpath="//a[contains(text(),'Адреса магазинов')]")
+
+	@FindBy(xpath = "//a[contains(text(),'Адреса магазинов')]")
 	private WebElement stores; // Адреса магазинов
-	
-	@FindBy(xpath="//a[contains(text(),'Арендодателям')]")
+
+	@FindBy(xpath = "//a[contains(text(),'Арендодателям')]")
 	private WebElement lease; // Арендодателям
-	
-	@FindBy(xpath="//a[contains(text(),'Юридическая информация')]")
+
+	@FindBy(xpath = "//a[contains(text(),'Юридическая информация')]")
 	private WebElement legal; // Юридическая информация
-	
-	@FindBy(xpath="//a[contains(text(),'Как сделать заказ на сайте')]")
+
+	@FindBy(xpath = "//a[contains(text(),'Как сделать заказ на сайте')]")
 	private WebElement howtobuy; // Как сделать заказ на сайте
-	
-	@FindBy(xpath="//a[contains(text(),'Оплата')]")
+
+	@FindBy(xpath = "//a[contains(text(),'Оплата')]")
 	private WebElement payment; // Оплата
-	
-	@FindBy(xpath="//a[contains(text(),'Доставка')]")
+
+	@FindBy(xpath = "//a[contains(text(),'Доставка')]")
 	private WebElement delivery; // Доставка
-	
-	@FindBy(xpath="//a[contains(text(),'Получение в магазине')]")
+
+	@FindBy(xpath = "//a[contains(text(),'Получение в магазине')]")
 	private WebElement pickup; // Получение в магазине
-	
-	@FindBy(xpath="//a[contains(text(),'Возврат и обмен')]")
+
+	@FindBy(xpath = "//a[contains(text(),'Возврат и обмен')]")
 	private WebElement exchange; // Возврат и обмен
-	
-	@FindBy(xpath="//a[contains(text(),'Правила работы')]")
+
+	@FindBy(xpath = "//a[contains(text(),'Правила работы')]")
 	private WebElement guide; // Правила работы
-	
-	@FindBy(xpath="//a[contains(text(),'Обратная связь')]")
+
+	@FindBy(xpath = "//a[contains(text(),'Обратная связь')]")
 	private WebElement feedback; // Обратная связь
-	
-	@FindBy(xpath="//div[@id='authorization']/span")
-	private WebElement menucabinet; // Меню Кабинет
-	
-	@FindBy(xpath="//a[contains(text(),'Войти на сайт')]")
-	private WebElement login; // Войти на сайт
-	
+
+	@FindBy(xpath = "//a[contains(text(),'Покупка в кредит')]")
+	private WebElement purchaseOnCredit; // Покупка в кредит
+
+	@FindBy(xpath = "//a[contains(text(),'Бонусная программа')]")
+	private WebElement bonusProgramm; // Бонусная программа
+
+	@FindBy(xpath = "//a[contains(text(),'Настройка и установка')]")
+	private WebElement setupAndInstallation; // Настройка и установка
+
+	@FindBy(xpath = "//a[contains(text(),'Подарочные карты')]")
+	private WebElement giftCards; // Подарочные карты
+
+	@FindBy(xpath = "//a[contains(text(),'Программа Сервис+')]")
+	private WebElement servicePlus; // Сервис плюс
+
+	@FindBy(xpath = "//a[contains(text(),'Страхование техники')]")
+	private WebElement insuranceTechniques; // Страхование техники
+
+	@FindBy(xpath = "//a[contains(text(),'Программа Технотренд')]")
+	private WebElement tehnotrendProgram; // Программа Технотренд
+
+	@FindBy(xpath = "//a[contains(text(),'Гарантийное обслуживание')]")
+	private WebElement warrantyService; // Гарантийное обслуживание
+
+	@FindBy(xpath = "//a[contains(text(),'Адреса сервисных центров')]")
+	private WebElement serviceCenterLocations; // Адреса сервисных центров
+
 	@Override
 	void tryToOpen() {
 		driver.get(this.URL_MATCH);
 	}
-	
+
 	// жмаканье на кнопку "Каталог товаров"
 	public void clickAllGoods() throws Exception {
 		try {
-			allgoods.click(); 
+			allgoods.click();
 			Log.info("Каталог товаров");
+		} catch (Exception e) {
+			Log.info("Element Not Found");
+			ScreenShot.takeScreenShot();
 		}
-	    catch(Exception e) {      
-	    	Log.info("Element Not Found");     
-            ScreenShot.takeScreenShot();       
-         }    
-	}	
-	
+	}
+
 	// жмаканье на кнопку "Телевизоры, аудио, видео"
 	public void clickTVAudioVideo() throws Exception {
 		try {
-			tvaudiovideo.click(); 
+			tvaudiovideo.click();
 			Log.info("Телевизоры, аудио, видео");
+		} catch (Exception e) {
+			Log.info("Element Not Found");
+			ScreenShot.takeScreenShot();
 		}
-	    catch(Exception e) {      
-	    	Log.info("Element Not Found");     
-            ScreenShot.takeScreenShot();       
-         }    
-	}	
-	
+	}
+
 	// жмаканье на кнопку "Телевизоры"
 	public void clickTV() throws Exception {
 		try {
-			tv.click(); 
+			tv.click();
 			Log.info("Телевизоры");
+		} catch (Exception e) {
+			Log.info("Element Not Found");
+			ScreenShot.takeScreenShot();
 		}
-	    catch(Exception e) {      
-	    	Log.info("Element Not Found");     
-            ScreenShot.takeScreenShot();       
-         }    
-	}	 
-	
+	}
+
 	// жмаканье на кнопку "LED телевизоры"
-	public void clickLEDTV() throws Exception {		
+	public void clickLEDTV() throws Exception {
 		try {
 			Actions builder = new Actions(driver);
-			Action dragAndDrop = builder.moveToElement(allgoods)
-					.moveToElement(tvaudiovideo)
-					.moveToElement(tv)
-					.moveToElement(ledtv)
-					.click(ledtv)
-						.release(ledtv)
-						.build();
+			Action dragAndDrop = builder.moveToElement(allgoods).moveToElement(tvaudiovideo).moveToElement(tv)
+					.moveToElement(ledtv).click(ledtv).release(ledtv).build();
 			dragAndDrop.perform();
 			Log.info("LED телевизоры");
+		} catch (Exception e) {
+			Log.info("Element Not Found");
+			ScreenShot.takeScreenShot();
 		}
-	    catch(Exception e) {      
-	    	Log.info("Element Not Found");     
-            ScreenShot.takeScreenShot();       
-         }    
 	}
-	
-// -----------------------------------------------------------------------------------------------------------------------------	
-	//ПОДВАЛ
-	
+
+	// -----------------------------------------------------------------------------------------------------------------------------
+	// ПОДВАЛ
+
 	// жмаканье на ссылку "О компании Техносила"
 	public void clickAboutTC() throws Exception {
 		try {
-			aboutTC.click(); 
+			aboutTC.click();
 			Log.info("О компании Техносила");
 			ScreenShot.takeScreenShot();
+		} catch (Exception e) {
+			Log.info("Element Not Found");
+			ScreenShot.takeScreenShot();
 		}
-	    catch(Exception e) {      
-	    	Log.info("Element Not Found");     
-            ScreenShot.takeScreenShot();       
-         }    
-	}	
-	
+	}
+
 	// жмаканье на ссылку "Новости компании"
 	public void clickNewsTC() throws Exception {
 		try {
-			newsTC.click(); 
+			newsTC.click();
 			Log.info("Новости компании");
 			ScreenShot.takeScreenShot();
+		} catch (Exception e) {
+			Log.info("Element Not Found");
+			ScreenShot.takeScreenShot();
 		}
-	    catch(Exception e) {      
-	    	Log.info("Element Not Found");     
-            ScreenShot.takeScreenShot();       
-         }    
 	}
-	
+
 	// жмаканье на ссылку "Вакансии"
 	public void clickCareer() throws Exception {
 		try {
-			career.click(); 
+			career.click();
 			Log.info("Вакансии");
 			ScreenShot.takeScreenShot();
+		} catch (Exception e) {
+			Log.info("Element Not Found");
+			ScreenShot.takeScreenShot();
 		}
-	    catch(Exception e) {      
-	    	Log.info("Element Not Found");     
-            ScreenShot.takeScreenShot();       
-         }    
 	}
-	
+
 	// жмаканье на ссылку "Пресс-релизы"
 	public void clickPress() throws Exception {
 		try {
-			press.click(); 
+			press.click();
 			Log.info("Пресс-релизы");
 			ScreenShot.takeScreenShot();
+		} catch (Exception e) {
+			Log.info("Element Not Found");
+			ScreenShot.takeScreenShot();
 		}
-	    catch(Exception e) {      
-	    	Log.info("Element Not Found");     
-            ScreenShot.takeScreenShot();       
-         }    
 	}
 
 	// жмаканье на ссылку "Контактная информация"
 	public void clickContacts() throws Exception {
 		try {
-			contacts.click(); 
+			contacts.click();
 			Log.info("Контактная информация");
 			ScreenShot.takeScreenShot();
+		} catch (Exception e) {
+			Log.info("Element Not Found");
+			ScreenShot.takeScreenShot();
 		}
-	    catch(Exception e) {      
-	    	Log.info("Element Not Found");     
-            ScreenShot.takeScreenShot();       
-         }    
 	}
-	
+
 	// жмаканье на ссылку "Адреса магазинов"
 	public void clickStores() throws Exception {
 		try {
-			stores.click(); 
+			stores.click();
 			Log.info("Адреса магазинов");
 			ScreenShot.takeScreenShot();
+		} catch (Exception e) {
+			Log.info("Element Not Found");
+			ScreenShot.takeScreenShot();
 		}
-	    catch(Exception e) {      
-	    	Log.info("Element Not Found");     
-            ScreenShot.takeScreenShot();       
-         }    
 	}
-	
+
 	// жмаканье на ссылку "Арендодателям"
 	public void clickLease() throws Exception {
 		try {
-			lease.click(); 
+			lease.click();
 			Log.info("Арендодателям");
 			ScreenShot.takeScreenShot();
+		} catch (Exception e) {
+			Log.info("Element Not Found");
+			ScreenShot.takeScreenShot();
 		}
-	    catch(Exception e) {      
-	    	Log.info("Element Not Found");     
-            ScreenShot.takeScreenShot();       
-         }    
 	}
-	
+
 	// жмаканье на ссылку "Юридическая информация"
 	public void clickLegal() throws Exception {
 		try {
-			legal.click(); 
+			legal.click();
 			Log.info("Юридическая информация");
 			ScreenShot.takeScreenShot();
+		} catch (Exception e) {
+			Log.info("Element Not Found");
+			ScreenShot.takeScreenShot();
 		}
-	    catch(Exception e) {      
-	    	Log.info("Element Not Found");     
-            ScreenShot.takeScreenShot();       
-         }    
 	}
-	
+
 	// жмаканье на ссылку "Как сделать заказ на сайте"
 	public void clickHowToBuy() throws Exception {
 		try {
-			howtobuy.click(); 
+			howtobuy.click();
 			Log.info("Как сделать заказ на сайте");
 			ScreenShot.takeScreenShot();
+		} catch (Exception e) {
+			Log.info("Element Not Found");
+			ScreenShot.takeScreenShot();
 		}
-	    catch(Exception e) {      
-	    	Log.info("Element Not Found");     
-            ScreenShot.takeScreenShot();       
-         }    
 	}
-	
+
 	// жмаканье на ссылку "Оплата"
 	public void clickPayment() throws Exception {
 		try {
-			payment.click(); 
+			payment.click();
 			Log.info("Оплата");
 			ScreenShot.takeScreenShot();
+		} catch (Exception e) {
+			Log.info("Element Not Found");
+			ScreenShot.takeScreenShot();
 		}
-	    catch(Exception e) {      
-	    	Log.info("Element Not Found");     
-            ScreenShot.takeScreenShot();       
-         }    
 	}
-	
+
 	// жмаканье на ссылку "Доставка"
 	public void clickDelivery() throws Exception {
 		try {
-			delivery.click(); 
+			delivery.click();
 			Log.info("Доставка");
 			ScreenShot.takeScreenShot();
+		} catch (Exception e) {
+			Log.info("Element Not Found");
+			ScreenShot.takeScreenShot();
 		}
-	    catch(Exception e) {      
-	    	Log.info("Element Not Found");     
-            ScreenShot.takeScreenShot();       
-         }    
 	}
-	
+
 	// жмаканье на ссылку "Получение в магазине"
 	public void clickPickup() throws Exception {
 		try {
-			pickup.click(); 
+			pickup.click();
 			Log.info("Получение в магазине");
 			ScreenShot.takeScreenShot();
+		} catch (Exception e) {
+			Log.info("Element Not Found");
+			ScreenShot.takeScreenShot();
 		}
-	    catch(Exception e) {      
-	    	Log.info("Element Not Found");     
-            ScreenShot.takeScreenShot();       
-         }    
 	}
-	
+
 	// жмаканье на ссылку "Возврат и обмен"
 	public void clickExchange() throws Exception {
 		try {
-			exchange.click(); 
+			exchange.click();
 			Log.info("Возврат и обмен");
 			ScreenShot.takeScreenShot();
+		} catch (Exception e) {
+			Log.info("Element Not Found");
+			ScreenShot.takeScreenShot();
 		}
-	    catch(Exception e) {      
-	    	Log.info("Element Not Found");     
-            ScreenShot.takeScreenShot();       
-         }    
 	}
-	
+
 	// жмаканье на ссылку "Правила работы"
 	public void clickGuide() throws Exception {
 		try {
-			guide.click(); 
+			guide.click();
 			Log.info("Правила работы");
 			ScreenShot.takeScreenShot();
+		} catch (Exception e) {
+			Log.info("Element Not Found");
+			ScreenShot.takeScreenShot();
 		}
-	    catch(Exception e) {      
-	    	Log.info("Element Not Found");     
-            ScreenShot.takeScreenShot();       
-         }    
 
 	}
+
 	// жмаканье на ссылку "Обратная связь"
 	public void clickFeedback() throws Exception {
 		try {
-			feedback.click(); 
+			feedback.click();
 			Log.info("Обратная связь");
 			ScreenShot.takeScreenShot();
+		} catch (Exception e) {
+			Log.info("Element Not Found");
+			ScreenShot.takeScreenShot();
+
 		}
-	    catch(Exception e) {      
-	    	Log.info("Element Not Found");     
-            ScreenShot.takeScreenShot();       
-      
-	    }
 	}
-	
-// -----------------------------------------------------------------------------------------------------------------------------
-	// Авторизация
-	
-	// жмаканье на меню "Кабинет"
-	public void clickMenuCabinet() throws Exception {
+
+	// УСЛУГИ И СЕРВИС
+
+	// клик по ссылке "Покупка в кредит"
+	public void clickPurchaseOnCredit() throws Exception {
 		try {
-			menucabinet.click(); 
-			Log.info("меню Кабинет");
+			purchaseOnCredit.click();
+			Log.info("Покупка в кредит");
+			ScreenShot.takeScreenShot();
+		} catch (Exception e) {
+			Log.info("Element Not Found");
+			ScreenShot.takeScreenShot();
 		}
-	    catch(Exception e) {      
-	    	Log.info("Element Not Found");     
-            ScreenShot.takeScreenShot();       
-         }    
 	}
-	
-	// жмаканье на "Войти на сайт"
-	public void clickLogIn() throws Exception {
+
+	// клик по ссылке "Бонусная программа"
+	public void clickBonusProgramm() throws Exception {
 		try {
-			login.click(); 
-			Log.info("Войти на сайт");
+			bonusProgramm.click();
+			Log.info("Бонусная программа");
+			ScreenShot.takeScreenShot();
+		} catch (Exception e) {
+			Log.info("Element Not Found");
+			ScreenShot.takeScreenShot();
 		}
-	    catch(Exception e) {      
-	    	Log.info("Element Not Found");     
-            ScreenShot.takeScreenShot();       
-         }    
 	}
+
+	// клик по ссылке "Настройка и установка"
+	public void clickSetupAndInstallation() throws Exception {
+		try {
+			setupAndInstallation.click();
+			Log.info("Настройка и установка");
+			ScreenShot.takeScreenShot();
+		} catch (Exception e) {
+			Log.info("Element Not Found");
+			ScreenShot.takeScreenShot();
+		}
+	}
+
+	// клик по ссылке "Подарочные карты"
+	public void clickGiftCards() throws Exception {
+		try {
+			giftCards.click();
+			Log.info("Подарочные карты");
+			ScreenShot.takeScreenShot();
+		} catch (Exception e) {
+			Log.info("Element Not Found");
+			ScreenShot.takeScreenShot();
+		}
+	}
+
+	// клик по ссылке "Сервис плюс"
+	public void clickServicePlus() throws Exception {
+		try {
+			servicePlus.click();
+			Log.info("Сервис плюс");
+			ScreenShot.takeScreenShot();
+		} catch (Exception e) {
+			Log.info("Element Not Found");
+			ScreenShot.takeScreenShot();
+		}
+	}
+
+	// клик по ссылке "Страхование техники"
+	public void clickInsuranceTechniques() throws Exception {
+		try {
+			insuranceTechniques.click();
+			Log.info("Страхование техники");
+			ScreenShot.takeScreenShot();
+		} catch (Exception e) {
+			Log.info("Element Not Found");
+			ScreenShot.takeScreenShot();
+		}
+	}
+
+	// клик по ссылке "Программа Технотренд"
+	public void clickTehnotrendProgram() throws Exception {
+		try {
+			tehnotrendProgram.click();
+			Log.info("Программа Технотренд");
+			ScreenShot.takeScreenShot();
+		} catch (Exception e) {
+			Log.info("Element Not Found");
+			ScreenShot.takeScreenShot();
+		}
+	}
+
+	// клик по ссылке "Гарантийное обслуживание"
+	public void clickWarrantyService() throws Exception {
+		try {
+			warrantyService.click();
+			Log.info("Гарантийное обслуживание");
+			ScreenShot.takeScreenShot();
+		} catch (Exception e) {
+			Log.info("Element Not Found");
+			ScreenShot.takeScreenShot();
+		}
+	}
+
+	// клик по ссылке "Адреса сервисных центров"
+	public void clickServiceCenterLocations() throws Exception {
+		try {
+			serviceCenterLocations.click();
+			Log.info("Адреса сервисных центров");
+			ScreenShot.takeScreenShot();
+		} catch (Exception e) {
+			Log.info("Element Not Found");
+			ScreenShot.takeScreenShot();
+		}
+	}
+
 }
