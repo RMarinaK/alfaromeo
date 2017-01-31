41ed55d112430617b49cee34af2634227e41e53c
RMarinaK <RasstriginaMK@tehnosila.ru>
2017-01-31 14:31:11 +0300

    Update Sys_getOrders.java

--- src/main/java/tehnosila/tehnosila_automation/pages/Sys_getOrders.java
+++ src/main/java/tehnosila/tehnosila_automation/pages/Sys_getOrders.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 
+import tehnosila.tehnosila_automation.AppManager.NavigationBase;
 import tehnosila.tehnosila_automation.AppManager.ScreenShot;
 
 /**
@@ -22,26 +23,57 @@ public class Sys_getOrders extends PagesBase{
 		
 	}
 	
+	@FindBy(xpath="//Invoice")
+	private WebElement invoicegid; // Номер заказа
+	
 	@FindBy(xpath="//Payment/Name") //
 	private WebElement paymentname; // Тип оплаты
 	
-	// вытягивание типа оплаты
-		public String getPaymentName(){
-				return paymentname.getText();
-		//	return paymentname.getAttribute("text");
-			}
-		
-		//	проверка отображения типа оплаты 
-		public void assertPaymentName(String paymentName) throws Exception {
-			try {
-				Assert.assertEquals(paymentName, getTextByJavascript(paymentname)); 
-				Log.info("***QA: Тип оплаты " +  getTextByJavascript(paymentname));
-			}
-		    catch(Exception e) {      
-		    	Log.info("Element Not Found");     
-	           ScreenShot.takeScreenShot();       
-	        }    
-		}	
+	@FindBy(xpath="//Delivery/name") //
+	private WebElement deliveryname; // Тип доставки
+	
+	
+	// вытягивание номера заказа
+	public String getID(){
+		return invoicegid.getAttribute("gid");
+	}
+	
+	//	проверка отображения типа оплаты 
+	public void assertOrders() throws Exception {
+		try {
+			Assert.assertEquals(NavigationBase.numberorder, getID()); 
+			Log.info("***QA: Номер заказа "+ getID());
+
+		}
+	    catch(Exception e) {      
+	    	Log.info("Element Not Found");     
+           ScreenShot.takeScreenShot();       
+        }      
+	}	
+	
+	//	проверка отображения типа оплаты 
+	public void assertPaymentName(String paymentName) throws Exception {
+		try {
+			Assert.assertEquals(paymentName, getTextByJavascript(paymentname)); 
+			Log.info("***QA: Тип оплаты " +  getTextByJavascript(paymentname));
+		}
+	    catch(Exception e) {      
+	    	Log.info("Element Not Found");     
+	          ScreenShot.takeScreenShot();       
+	    }    
+	}	
+	
+	//	проверка отображения типа доставки
+	public void assertDeliveryName(String deliveryName) throws Exception {
+		try {
+			Assert.assertEquals(deliveryName, getTextByJavascript(deliveryname)); 
+			Log.info("***QA: Тип доставки " +  getTextByJavascript(deliveryname));
+		}
+	    catch(Exception e) {      
+	    	Log.info("Element Not Found");     
+	          ScreenShot.takeScreenShot();       
+	    }    
+	}	
 	
 	
 }
