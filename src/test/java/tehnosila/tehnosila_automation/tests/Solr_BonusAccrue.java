d3d89588478b43c2dfdbb00db443a3dda1c8b099
ElDanilova <DanilovaEA@tehnosila.ru>
2017-05-30 10:45:05 +0300

    Попытка настроить запуск с терминала

--- src/test/java/tehnosila/tehnosila_automation/tests/Solr_BonusAccrue.java
+++ src/test/java/tehnosila/tehnosila_automation/tests/Solr_BonusAccrue.java
@@ -48,7 +48,7 @@ public class Solr_BonusAccrue extends TestBase{
 				+ NavigationBase.psolrpickupAvailabilityTyp + NavigationBase.psolrtail);
 		CommonMetods commonmetods = MyPageFactory.getPage(CommonMetods.class);
 		commonmetods.getHTTPResponseCode();
-		app.getNavigationHelper().getCodeString();
+		app.getGetDataHelper().getCodeString();
 		Page_Tehnosila pagetehnosila = MyPageFactory.getPage(Page_Tehnosila.class);
 		pagetehnosila.getPage();
 		commonmetods.getHTTPResponseCode();
